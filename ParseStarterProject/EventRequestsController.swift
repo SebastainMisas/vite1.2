dc0036383ec6aaaa79cff0722f2300f86c53dfbc
sebastian <juanmisas2@gmail.com>
2016-02-01 19:59:36 -0500

    updated UI and constraints

---
+++ ParseStarterProject/EventRequestsController.swift
@@ -0,0 +1,13 @@
+//
+//  EventRequestsController.swift
+//  vite1.1
+//
+//  Created by Sebastian Misas on 2/1/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import Cocoa
+
+class EventRequestsController: UIViewController {
+
+}
