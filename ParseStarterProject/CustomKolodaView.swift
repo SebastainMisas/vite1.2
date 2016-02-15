f39f9b6652ce26b652a6924434001cc2b59079ad
Sebastian Misas <juanmisas2@gmail.com>
2016-02-15 01:35:04 -0500

    added firebase and koloda view for swipe

---
+++ ParseStarterProject/CustomKolodaView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomKolodaView.swift
+//  vite1.1
+//
+//  Created by Sebastian Misas on 2/15/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import Foundation
---
+++ ParseStarterProject/SwipeView.swift
@@ -0,0 +1,9 @@
+//
+//  SwipeView.swift
+//  vite1.1
+//
+//  Created by Sebastian Misas on 2/15/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import Foundation
